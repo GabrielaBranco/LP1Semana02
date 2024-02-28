dbd993e122a9494117e3218292d9d4918c0d59f2
Gabriela Branco <lindosogb@gmail.com>
2024-02-28 18:56:04 +0000

    add new user input for creating a cylinder

---
+++ Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the height of a cylinder:");
+            string altura = Console.ReadLine();
+            float a = float.Parse(altura); 
+            
+            Console.WriteLine("Give me the radius of the cylinder:");
+            string raio = Console.ReadLine();
+            float r = float.Parse(raio);
+
+            Console.WriteLine(MathF.PI * MathF.Pow(r,2) * a);
+            Console.WriteLine(MathF.PI * 2 * r * (r + a));
+
+        }
+    }
+}
