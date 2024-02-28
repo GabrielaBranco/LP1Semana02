831098c7869b76b629d9400e9bbcebcbbe4688c3
Gabriela Branco <lindosogb@gmail.com>
2024-02-28 18:35:58 +0000

    add new user input for asking non negative numbers

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a non-negative integer:");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            Console.WriteLine(num - 1);
+            Console.WriteLine(num);
+            Console.WriteLine(num / 2);
+            Console.WriteLine(num << 3);
+            Console.WriteLine(num ^ 5);
+
+            bool n = num > 10;
+            Console.WriteLine(n);
+        }
+    }
+}
